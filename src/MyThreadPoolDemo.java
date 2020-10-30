c61eaedd7e69f03b959b99a9ce9c3d004f597eab
汤建国 <427708835@qq.com>
2020-10-30 20:44:24 +0800

    juc的一些小demo

--- src/MyThreadPoolDemo.java
+++ src/MyThreadPoolDemo.java
@@ -2,6 +2,7 @@
 
 import java.util.concurrent.*;
 
+//手写线程池
 public class MyThreadPoolDemo {
     public static void main(String[] args) {
        ExecutorService threadpool = new ThreadPoolExecutor(2,
@@ -29,22 +30,22 @@ public class MyThreadPoolDemo {
 
 
 
-
-    private static void test() {
-        ExecutorService threadpool = Executors.newFixedThreadPool(3);
-        ExecutorService threadpoo2 = Executors.newSingleThreadExecutor();
-        ExecutorService threadpoo3 = Executors.newCachedThreadPool();
-
-        try {
-            for (int i = 1; i <=30 ; i++) {
-                threadpoo3.execute(()->{
-                    System.out.println(Thread.currentThread().getName()+"业务员执行任务");
-                });
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            threadpoo3.shutdown();
-        }
-    }
+//
+//    private static void test() {
+//        ExecutorService threadpool = Executors.newFixedThreadPool(3);
+//        ExecutorService threadpoo2 = Executors.newSingleThreadExecutor();
+//        ExecutorService threadpoo3 = Executors.newCachedThreadPool();
+//
+//        try {
+//            for (int i = 1; i <=30 ; i++) {
+//                threadpoo3.execute(()->{
+//                    System.out.println(Thread.currentThread().getName()+"业务员执行任务");
+//                });
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            threadpoo3.shutdown();
+//        }
+//    }
 }
--- src/MyThreadPoolDemo1.java
+++ src/MyThreadPoolDemo1.java
@@ -1,2 +1,25 @@
+import java.util.concurrent.*;
+
 public class MyThreadPoolDemo1 {
+
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(3,
+                5,
+                12L,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardPolicy());
+
+
+        for (int i = 0; i <10 ; i++) {
+            pool.execute(()->{
+                System.out.println(Thread.currentThread().getName()+"号业务员操作业务");
+            });
+        }
+
+        pool.shutdown();
+
+    }
 }
